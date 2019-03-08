27a3f81099fb8b1c37727f7f82c6701bb538e398
MateusAlmeidaTI <mateusalmeida.ti@gmail.com>
2019-03-08 02:04:51 -0300

    Primeira Missao edit JS para HEAD

---
+++ Missao Estelar/Primeira Missao/MateusAlmeidaTI/JS/js.js
@@ -0,0 +1,10 @@
+var missão = "Missão Estelar JS em Execução!"
+
+function eventoOnload(){
+    console.log("Evento Onload Disparado!")
+    }
+
+function eventoOnclick(){
+    console.log ("Missão Estelar JS realizada com sucesso!")
+}
+
